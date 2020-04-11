8b521ef1c450d2086d3b2346fe4178bfae952899
bear <ki89ol>
2020-04-11 22:07:37 +0800

    commit

--- src/main/java/com/lichun/swagger/Application.java
+++ src/main/java/com/lichun/swagger/Application.java
@@ -1,6 +1,5 @@
 package com.lichun.swagger;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/lichun/swagger/SwaggerConfig.java
+++ src/main/java/com/lichun/swagger/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.lichun.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,9 +21,13 @@ public class SwaggerConfig {
      * 函数名,可以随意命名,喜欢什么命名就什么命名
      */
     @Bean
-    public Docket createRestApi() {
+    public Docket createRestApi(Environment environment) {
+        Profiles profiles = Profiles.of("dev", "test");
+        //判断是否处于自己设定的环境中
+        boolean flag = environment.acceptsProfiles(profiles);
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())//调用apiInfo方法,创建一个ApiInfo实例,里面是展示在文档页面信息内容
+                .enable(flag)
                 .select()
                 //控制暴露出去的路径下的实例
                 //如果某个接口不想暴露,可以使用以下注解
